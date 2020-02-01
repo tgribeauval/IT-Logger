5e37861b50e853da43a5d85f5c8e549c5b091a3b
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-02-01 00:54:02 -0500

    Edit and Tech Modal Components

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddLogModal = props => {
+const EditLogModal = props => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -23,7 +23,7 @@ const AddLogModal = props => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
       <div className='modal-content'>
         <h4>Enter System Log</h4>
         <div className='row'>
@@ -89,4 +89,4 @@ const modalStyle = {
   height: "75%"
 };
 
-export default AddLogModal;
+export default EditLogModal;
